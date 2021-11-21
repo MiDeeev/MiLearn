d59f3bec5035d25f7a299725c0de42aa43f7dd88
MiDeev <everylights27@gmail.com>
2021-11-21 23:36:58 +0400

    third commit

--- src/ru/mideev/jalearn/Main.java
+++ src/ru/mideev/jalearn/Main.java
@@ -2,6 +2,6 @@ package ru.mideev.jalearn;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit?");
+        System.out.println("Third");
     }
 }
