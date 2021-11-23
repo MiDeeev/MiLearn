3e2f0f8501e606cb97ed6e477cdfedd139d6f603
MiDeev <everylights27@gmail.com>
2021-11-23 23:50:18 +0400

    pre-learn commit

--- src/ru/mideev/jalearn/Main.java
+++ src/ru/mideev/jalearn/Main.java
@@ -1,7 +1,16 @@
 package ru.mideev.jalearn;
 
+/**
+ * @author MiDeev
+ * <p>Привет, здесь я по всей видимости буду изучать Java.</p>
+ * <p>Именно отсюда я начинаю свой путь программирования на этом языке, и я надеюсь, что из этого что-то выйдет.</p>
+ * <p>Let's go!</p>
+**/
+
+//далее будет кринж и русский язык на англоязычном GitHub, ну ничего страшного.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Third");
+        System.out.println("pre-learn!");
     }
 }
