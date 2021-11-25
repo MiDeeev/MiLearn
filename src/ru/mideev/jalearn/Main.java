3c4411035144b4fd6d9691830daae14a64f54b8f
MiDeev <everylights27@gmail.com>
2021-11-25 23:53:45 +0400

    "sorting"

--- src/ru/mideev/jalearn/Main.java
+++ src/ru/mideev/jalearn/Main.java
@@ -4,16 +4,6 @@ package ru.mideev.jalearn;
 
 public class Main {
     public static void main(String[] args) {
-
-        // начнём с примитивных типов данных:
-
-        byte a = 127; // от -128 до 127
-        short b = 32767; // от -32768 до 32767
-        int c = 2147483647; // от -2147483648 до 2147483647
-        long d = 9223372036854775807L; // от -9223372036854775808L до 9223372036854775807L
-        double e = 2.5; // от ±4.9*10-324 до ±1.8*10308
-        float f = 1.01F; // от -3.4*1038 до 3.4*1038
-        char j = 'j'; // от 0 до 65535 & UTF-16
-        boolean h = true; // true / false
+        //temporarily unused main class...
     }
 }
